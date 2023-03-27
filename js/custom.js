3494e3998d4a4440e105e1883ec429f0118b479a
Stevixent <chiadikavirtue@gmail.com>
2023-03-27 18:19:48 +0100

    ok

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-// coding with nick
 
 // Js Documents
 
@@ -12,3 +11,111 @@
 
 
 
+
+jQuery(document).ready(function($)
+{
+     "user strict";
+
+     // vars and Inits
+
+     var mainSlider = $('.main_slider');
+     var hamburger = $('.hamburger_container');
+     var menu = $('.hamburger_menu');
+     var menuActive = false;
+     var hamburgerClose = $('.hamburger_close');
+     var fsOverlay = $('.fs_menu_overlay');
+
+
+     initFavorite();
+
+
+// 2.  Inits Menu
+
+
+// 3.  Init Timer
+
+
+
+// 4.  Init Favorite
+function initFavorite()
+{
+    if($('.favorite').length)
+    {
+        var favs = $('.favorite');
+
+        favs.each(function()
+        {
+            var fav = $(this);
+            var active = false;
+            if(fav.hasClass('active'))
+            {
+                active = true;
+            }
+            fav.on('click', function()
+            {
+                if (active)
+                {
+                   fav.removeClass('active');
+                   active = false 
+                }
+                else
+                {
+                    fav.addClass('active');
+                    active = true
+                }
+            });
+        });
+    }
+}
+
+
+// 5.  Init Isotope Filtering
+
+
+
+// 6.  Init Slider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
