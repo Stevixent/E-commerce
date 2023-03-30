4f1a4d22c633ddf3d75411967dbb6e7a3eb33fec
Stevixent <chiadikavirtue@gmail.com>
2023-03-30 18:05:02 +0100

    ok

--- js/custom.js
+++ js/custom.js
@@ -28,6 +28,7 @@ jQuery(document).ready(function($)
 
      initFavorite();
      initIsotopeFiltering();
+     initTimer();
 
 
 // 2.  Inits Menu
@@ -35,6 +36,46 @@ jQuery(document).ready(function($)
 
 // 3.  Init Timer
 
+function initTimer()
+{
+    if ($('.timer').length) 
+    {
+     
+        var date = new Date();
+        date.setDate(date.getDate() + 3);
+        var target_date = date.getTime();
+
+
+        // variable for time units
+        var days, hours, minutes, seconds;
+        var d = $('#day');
+        var h = $('#hour');
+        var m = $('#minute');
+        var s = $('#second');
+
+        setInterval(function () 
+        {
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            //display result
+            d.text(days);
+            h.text(hours);
+            m.text(minutes);
+            s.text(seconds);
+            
+        }, 1000);
+    }
+}
 
 
 // 4.  Init Favorite
